13cb26d8563690c4cbe268a97b052dbb1f553319
Alex Ellis (VMware) <alexellis2@gmail.com>
2019-01-09 15:28:52 +0000

    Initial
    Initial working example triggering with a minimum set of data
    and examples in README.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # vcenter-connector
+
 OpenFaaS event connector for VMware vCenter
+
+## Status
+
+This project connects VMware vCenter events to OpenFaaS functions via topics applied in the "topic" annotation. 
+
+It is build using the [OpenFaaS Connector SDK](https://github.com/openfaas-incubator/connector-sdk)
+
+The code is under active development and only suitable for testing at this point.
+
+## Example:
+
+* Run the vCenter Simulator
+
+```bash
+ ./vcsim -tls=false
+```
+
+* Run the connector
+
+```bash
+export OPENFAAS_URL=http://127.0.0.1:31112
+go run main.go -vcenter-url="http://user:pass@127.0.0.1:8989/sdk" -insecure
+```
+
+Deploy an echo function that subscribes to the event of "vm.powered.on"
+
+```bash
+faas-cli deploy --annotation topic="vm.powered.on" --fprocess=/bin/cat -e write_debug=true --image=functions/alpine:latest --name vmware
+```
+
+* Generate some events:
+
+```
+GOVC_INSECURE=true GOVC_URL=http://user:pass@127.0.0.1:8989/sdk govc vm.power -off '*'
+```
+
+* Check the logs of the vmware function
+
+```
+kubectl logs -n openfaas-fn deploy/vmware
+docker service logs vmware
+```
+
+## License
+
+MIT
+
+## Acknowledgements
+
+Thanks to VMware's Doug MacEachern for the awesome [govmomi](https://github.com/vmware/govmomi) project providing Golang bindings for vCenter and the [vcsim simulator tool](https://github.com/vmware/govmomi/blob/master/vcsim/README.md).
+
+Thanks to Karol Stępniewski for showing me a demo of events being consumed in OpenFaaS via vCenter over a year ago at KubeCon in Austin. Parts of his "event-driver" originally developed in the Dispatch project have been adapted for this OpenFaaS event-connector including a method to convert camel case event names into names separated by a dot. I wanted to include this for compatibility between the two systems.
---
+++ main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"reflect"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/openfaas-incubator/connector-sdk/types"
+	"github.com/openfaas/faas-provider/auth"
+	"github.com/pkg/errors"
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/event"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
+	vtypes "github.com/vmware/govmomi/vim25/types"
+)
+
+func main() {
+	var vcenterURL string
+	var insecure bool
+
+	flag.StringVar(&vcenterURL, "vcenter-url", "", "URL for vcenter user:password@domain.com:port")
+	flag.Bool("insecure", true, "use an insecure connection")
+	flag.Parse()
+
+	if len(vcenterURL) == 0 {
+		panic("vcenterURL not provided")
+	}
+
+	vcenterClient, vcenterErr := newVCenterClient(context.Background(), vcenterURL, insecure)
+
+	if vcenterErr != nil {
+		panic(vcenterErr)
+	}
+
+	var credentials *auth.BasicAuthCredentials
+
+	if val, ok := os.LookupEnv("basic_auth"); ok && len(val) > 0 {
+		if val == "true" || val == "1" {
+
+			reader := auth.ReadBasicAuthFromDisk{}
+
+			if val, ok := os.LookupEnv("secret_mount_path"); ok && len(val) > 0 {
+				reader.SecretMountPath = os.Getenv("secret_mount_path")
+			}
+
+			res, err := reader.Read()
+			if err != nil {
+				panic(err)
+			}
+			credentials = res
+		}
+	}
+
+	config := types.ControllerConfig{
+		GatewayURL:      os.Getenv("OPENFAAS_URL"),
+		PrintResponse:   true,
+		RebuildInterval: time.Second * 10,
+		UpstreamTimeout: time.Second * 15,
+	}
+
+	controller := types.NewController(credentials, &config)
+
+	controller.BeginMapBuilder()
+
+	err := bindEvents(vcenterClient.Client, controller)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func bindEvents(c *vim25.Client, controller *types.Controller) error {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m := event.NewManager(c)
+	managedTypes := []vtypes.ManagedObjectReference{c.ServiceContent.RootFolder}
+	eventsPerPage := int32(1)
+	tail := true
+	force := true
+
+	recv := makeRecv(controller, m)
+	go func() {
+		err := m.Events(ctx, managedTypes, eventsPerPage, tail, force, recv)
+		if err != nil {
+			log.Printf("error connecting to event-stream: %v", err.Error())
+			cancel()
+		}
+	}()
+
+	done := make(chan bool)
+
+	<-done
+
+	// controller.Invoke()
+
+	return nil
+}
+
+func makeRecv(controller *types.Controller, m *event.Manager) func(managedObjectReference vtypes.ManagedObjectReference, baseEvent []vtypes.BaseEvent) error {
+	return func(managedObjectReference vtypes.ManagedObjectReference, baseEvent []vtypes.BaseEvent) error {
+		log.Printf("Object %v", managedObjectReference)
+
+		for i, event := range baseEvent {
+			log.Printf("Event [%d] %v", i, event)
+
+			topic, message, err := handleEvent(event, m)
+			if err != nil {
+				log.Printf("error handling event: %s", err.Error())
+				continue
+			}
+			binaryMsg := []byte(message)
+			log.Printf("Message on topic: %s", topic)
+			controller.Invoke(topic, &binaryMsg)
+
+		}
+		return nil
+	}
+}
+
+func handleEvent(event vtypes.BaseEvent, m *event.Manager) (string, string, error) {
+	eventType := reflect.TypeOf(event).Elem().Name()
+
+	category, err := m.EventCategory(context.Background(), event)
+	if err != nil {
+		return "", "", errors.Wrap(err, "error retrieving event category")
+	}
+
+	log.Printf("category: %v, event type: %v", category, eventType)
+
+	topic := convertToTopic(eventType)
+
+	message, _ := json.Marshal(OutboundEvent{
+		Topic:    topic,
+		Category: category,
+	})
+
+	return topic, string(message), nil
+}
+
+type OutboundEvent struct {
+	Topic    string
+	Category string
+}
+
+func newVCenterClient(ctx context.Context, vcenterURL string, insecure bool) (*govmomi.Client, error) {
+	url, err := soap.ParseURL(vcenterURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return govmomi.NewClient(ctx, url, insecure)
+}
+
+func convertToTopic(eventType string) string {
+
+	eventType = strings.Replace(eventType, "Event", "", -1)
+	return camelCaseToLowerSeparated(eventType, ".")
+}
+
+// From https://github.com/vmware/dispatch/blob/master/pkg/utils/str_convert.go
+// camelCaseToLowerSeparated converts a camel cased string to a multi-word string delimited by the specified separator
+func camelCaseToLowerSeparated(src string, sep string) string {
+	var words []string
+	var word []rune
+	var last rune
+	for _, r := range src {
+		if unicode.IsUpper(r) {
+			if unicode.IsUpper(last) {
+				// We have two uppercase letters in a row, it might be uppercase word like ID or SDK
+				word = append(word, r)
+			} else {
+				// We have uppercase after lowercase, which always means start of a new word
+				if len(word) > 0 {
+					words = append(words, strings.ToLower(string(word)))
+				}
+				word = []rune{r}
+			}
+		} else {
+			if unicode.IsUpper(last) && len(word) >= 2 {
+				// We have a multi-uppercase word followed by an another word, e.g. "SDKToString",
+				// but word variable contains "SDKT". We need to extract "T" as a first letter of a new word
+				words = append(words, strings.ToLower(string(word[:len(word)-1])))
+				word = []rune{last, r}
+			} else {
+				word = append(word, r)
+			}
+		}
+		last = r
+	}
+	if len(word) > 0 {
+		words = append(words, strings.ToLower(string(word)))
+	}
+	return strings.Join(words, sep)
+}
